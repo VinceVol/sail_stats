3fb2de1052d8b09e2323866a09b18bd6844a1671
Vincent Volpone <vincent.volpone@gmail.com>
2025-09-10 07:02:46 -0400

    realized the vast majority of what I chased all night was labeling the scl and sda pins from the usb controllers internal i2c (rip) and also learned that once the i2c peripheral is configured that a driver is used (or needs to be created) to get useful information

--- src/heel.rs
+++ src/heel.rs
@@ -1,12 +1,20 @@
+use core::any::Any;
+
+use defmt::dbg;
 use defmt::info;
 use embassy_nrf::{
     bind_interrupts,
-    gpio::AnyPin,
-    peripherals::{P0_28, P0_30, TWISPI0},
+    peripherals::{P0_08, P0_16, TWISPI0},
     twim, Peri,
 };
+use embassy_time::Delay;
+use embassy_time::Timer;
+use lsm303agr::AccelOutputDataRate;
+use lsm303agr::Lsm303agr;
 use static_cell::ConstStaticCell;
 
+const ACCEL_ADDY: u8 = 0x19;
+const MAG_ADDY: u8 = 0x1E;
 //Having a pretty hard time determining the difference between TWISPI0 and TWISPI1, to the best of my knowledge they are both interrupts associated with using TWI (I2C)
 //if we have problems with the accelerometer I'll swithc this to TWISPI1 to see if it makes a difference
 bind_interrupts!(
@@ -16,15 +24,38 @@ bind_interrupts!(
 );
 
 #[embassy_executor::task]
-async fn init_heel(
+pub async fn init_heel(
     twi_p: Peri<'static, TWISPI0>,
-    scl_p: Peri<'static, P0_28>,
-    sda_p: Peri<'static, P0_30>,
+    scl_p: Peri<'static, P0_08>,
+    sda_p: Peri<'static, P0_16>,
 ) {
     info!("Initializing accelorometer twi...");
     let config = twim::Config::default();
 
-    //need to determine if a buffer is really required for our use case
-    static RAM_BUFFER: static_cell::ConstStaticCell<[u8; 16]> = ConstStaticCell::new([0; 16]);
+    //I think buffer size is just null because it has to do with size of write which we'll only be reading
+    static RAM_BUFFER: static_cell::ConstStaticCell<[u8; 0]> = ConstStaticCell::new([0; 0]);
     let mut twi = twim::Twim::new(twi_p, Irqs, sda_p, scl_p, config, RAM_BUFFER.take());
+
+    let mut rx_buf = [0u8; 16];
+    info!("Reading...");
+
+    //Following https://github.com/eldruin/lsm303agr-rs/blob/HEAD/examples/microbit-v2.rs this example for implementing accelorometer driver
+    let mut sensor = Lsm303agr::new_with_i2c(twi);
+    sensor.init().unwrap();
+    sensor
+        .set_accel_mode_and_odr(
+            &mut Delay,
+            lsm303agr::AccelMode::LowPower,
+            AccelOutputDataRate::Hz10,
+        )
+        .unwrap();
+    loop {
+        //This loop is obsolete once driver is fully implemented
+        let res = twi.read(ACCEL_ADDY, &mut rx_buf).await;
+        match res {
+            Ok(T) => info!("Read: {=[u8]:x}", rx_buf),
+            Err(E) => info!("Error reading accelorometer: {:?}", E),
+        }
+        Timer::after_millis(400).await;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,56 @@
 mod fmt;
 mod heel;
 
+use defmt::dbg;
 #[cfg(not(feature = "defmt"))]
 use panic_halt as _;
 #[cfg(feature = "defmt")]
 use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_nrf::gpio::{AnyPin, Input, Level, Output, OutputDrive};
 use embassy_time::Timer;
 use fmt::info;
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
     let mut led = Output::new(p.P0_13, Level::Low, OutputDrive::Standard);
+
+    let _ = spawner.spawn(button(p.P0_14.into(), ButtonSide::A));
+    let _ = spawner.spawn(button(p.P0_23.into(), ButtonSide::B));
+    let res = spawner.spawn(heel::init_heel(p.TWISPI0, p.P0_08, p.P0_16));
+    dbg!(res);
 
     loop {
         info!("Hello, World!");
         led.set_high();
-        Timer::after_millis(500).await;
+        Timer::after_secs(3).await;
         led.set_low();
-        Timer::after_millis(500).await;
+        Timer::after_secs(3).await;
+    }
+}
+
+//Putting this as a gut check to make sure the board is working and interacting
+enum ButtonSide {
+    A,
+    B,
+}
+
+#[embassy_executor::task(pool_size = 2)]
+async fn button(pin: embassy_nrf::Peri<'static, AnyPin>, button_side: ButtonSide) {
+    let mut button = Input::new(pin, embassy_nrf::gpio::Pull::None);
+    loop {
+        button.wait_for_low().await;
+        match button_side {
+            ButtonSide::A => {
+                info!("you've pressed button a!");
+            }
+            ButtonSide::B => {
+                info!("you've pressed button b!");
+            }
+        }
+        embassy_time::Timer::after_millis(150).await;
+        button.wait_for_high().await;
     }
 }
