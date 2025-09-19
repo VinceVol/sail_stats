41f417ea22c913d78b5030ed8a634532d7007500
VinceV <vincent.volpone@gentex.com>
2025-09-19 13:56:07 -0400

    finished implementing wrapper for sdmmca and need to wait to plug it in and test

--- src/micro_sd.rs
+++ src/micro_sd.rs
@@ -2,19 +2,16 @@
 //storage for the user to then take to their computer and crunch the
 //results
 
-use core::{
-    borrow::{Borrow, BorrowMut},
-    fmt::Debug,
-};
-
 use defmt::info;
 use embassy_nrf::{
     bind_interrupts,
-    peripherals::{P0_01, P0_13, P0_17, RTC0, SPI2},
+    peripherals::{P0_01, P0_13, P0_17, SPI2},
     spim, Peri,
 };
 use embassy_time::Delay;
-use embedded_sdmmc::{Error, Mode, SdCard, SdCardError, TimeSource, Timestamp, VolumeIdx, VolumeManager};
+use embedded_sdmmc::{
+    Error, Mode, SdCard, SdCardError, TimeSource, Timestamp, VolumeIdx, VolumeManager,
+};
 
 //Just like I2C not really sure what the difference between spi2 and spi3 is
 //and when to use one or the other
@@ -29,7 +26,6 @@ pub async fn init_save(
     mosi_p: Peri<'static, P0_13>,
     sck_p: Peri<'static, P0_17>,
     cs_p: embassy_nrf::gpio::Output<'static>,
-    timesrc_p: Peri<'static, RTC0>,
 ) {
     //Init SPIM --> may need to be something we do in main, especially if we want
     //to share the SPI bus
@@ -43,30 +39,48 @@ pub async fn init_save(
     let sdcard = SdCard::new(exclusive_spi, Delay);
 
     info!("Card size is {} bytes", sdcard.num_bytes().unwrap());
-    let volume_mgr = VolumeManager::new(sdcard, timesrc_p);
-    let volume0 = volume_mgr.open_volume(VolumeIdx(0)).unwrap();
-    info!("Volume 0: {:?}", volume0);
-    let root_dir = volume0.open_root_dir().unwrap();
-    info!("we actually made it here!");
+    let time_source = RTCWrapper::new();
+    let volume_mgr = VolumeManager::new(sdcard, time_source);
+    let volume0 = volume_mgr.open_volume(VolumeIdx(0));
+    if volume0.is_ok_and(|v| v.open_root_dir().is_ok()) {
+        info!("Root dir success!");
+    } else {
+        info!("Root dir failure!");
+    }
 }
 
+//Not sure what to contain within the struct given that the methods surrounding this wrapper
+// are initialized in main || using var functional to call out whether the embassy time crate
+// is currently functioning
 struct RTCWrapper {
-    rtc: Peri<'static, RTC0>,
+    functional: bool,
+}
+
+impl RTCWrapper {
+    fn new() -> RTCWrapper {
+        //find TEST for whether or not embassy time is working properly
+        RTCWrapper { functional: true }
+    }
 }
 
 //need a shitty function to convert ticks from embassy_time to hour/min/sec it's a fair
 //assumption to say this sailing tracker wouldn't run on the dinghy for over 24hrs
-fn tick_to_time(ticks: u64) -> (u8, u8, u8) {
-    let hours:f64 = (ticks as f64)/32768f64;
+fn sec_to_time(seconds: u64) -> (u8, u8, u8) {
+    let hours: f64 = (seconds as f64) / (60f64 * 60f64);
     let minutes = (hours - ((hours as u8) as f64)) * 60f64;
-    let seconds = (minutes - ((minutes as u8) as f64)) * 60f64;    
-    return (hours as u8,minutes as u8,seconds as u8);
+    let seconds = (minutes - ((minutes as u8) as f64)) * 60f64;
+    return (hours as u8, minutes as u8, seconds as u8);
 }
 
-impl  TimeSource for RTCWrapper {
+impl TimeSource for RTCWrapper {
     fn get_timestamp(&self) -> embedded_sdmmc::Timestamp {
-        let now = embassy_time::Instant::now();
-        let (hour,min,sec) = tick_to_time(now);
-        let time_stamp = Timestamp::from_calendar(2025,9 ,19 ,6 hour, min, sec);
+        let now = embassy_time::Instant::now().as_secs();
+        let (hour, min, sec) = sec_to_time(now);
+        if let Ok(time_stamp) = Timestamp::from_calendar(2025, 9, 19, hour, min, sec) {
+            return time_stamp;
+        } else {
+            //Guaranteed realed time (SHOULD TEST TODO)
+            return Timestamp::from_calendar(2025, 7, 28, 0, 0, 0).unwrap();
+        }
     }
 }
