b8c07f4e6aff5927d26fb025c1ca43b83e7034b4
Vincent Volpone <vincent.volpone@gmail.com>
2025-09-19 06:58:42 -0400

    working on a wrapper for TimeSource to communicate with my micro SD card

--- src/micro_sd.rs
+++ src/micro_sd.rs
@@ -2,14 +2,19 @@
 //storage for the user to then take to their computer and crunch the
 //results
 
+use core::{
+    borrow::{Borrow, BorrowMut},
+    fmt::Debug,
+};
+
 use defmt::info;
 use embassy_nrf::{
     bind_interrupts,
-    peripherals::{P0_01, P0_13, P0_17, SPI2},
+    peripherals::{P0_01, P0_13, P0_17, RTC0, SPI2},
     spim, Peri,
 };
 use embassy_time::Delay;
-use embedded_sdmmc::{Error, Mode, SdCard, SdCardError, TimeSource, VolumeIdx, VolumeManager};
+use embedded_sdmmc::{Error, Mode, SdCard, SdCardError, TimeSource, Timestamp, VolumeIdx, VolumeManager};
 
 //Just like I2C not really sure what the difference between spi2 and spi3 is
 //and when to use one or the other
@@ -42,4 +47,25 @@ pub async fn init_save(
     info!("Volume 0: {:?}", volume0);
     let root_dir = volume0.open_root_dir().unwrap();
     info!("we actually made it here!");
+}
+
+struct RTCWrapper {
+    rtc: Peri<'static, RTC0>,
+}
+
+//need a shitty function to convert ticks from embassy_time to hour/min/sec it's a fair
+//assumption to say this sailing tracker wouldn't run on the dinghy for over 24hrs
+fn tick_to_time(ticks: u64) -> (u8, u8, u8) {
+    let hours:f64 = (ticks as f64)/32768f64;
+    let minutes = (hours - ((hours as u8) as f64)) * 60f64;
+    let seconds = (minutes - ((minutes as u8) as f64)) * 60f64;    
+    return (hours as u8,minutes as u8,seconds as u8);
+}
+
+impl  TimeSource for RTCWrapper {
+    fn get_timestamp(&self) -> embedded_sdmmc::Timestamp {
+        let now = embassy_time::Instant::now();
+        let (hour,min,sec) = tick_to_time(now);
+        let time_stamp = Timestamp::from_calendar(2025,9 ,19 ,6 hour, min, sec);
+    }
 }
