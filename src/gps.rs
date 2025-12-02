691cbfd5f68fe0569c96e1419de83a4067169023
Vincent Volpone <vincent.volpone@gmail.com>
2025-12-02 06:55:16 -0500

    Finished most of the math surrounding gpgga, opted to start a new function in case any of the parsing returned errors, need to move all info into the gps struct and zap that to the sd card

--- src/gps.rs
+++ src/gps.rs
@@ -14,10 +14,11 @@ use embassy_nrf::{
 //baud rate for arduino gps is defaulted to 9600
 //GPGGA,113727.00,4303.16727,N,08612.65632,W,1,07,1.43,197.6,M,-34.5,M,,*60
 struct Gps {
-    deg_north: f32,
-    deg_west: f32,
-    time: f32,
-    date: f32,
+    deg_north: BufTxt,
+    deg_west: BufTxt,
+    date_time: BufTxt, //utc time
+    altitude: BufTxt,
+    altitude_units: BufTxt,
 }
 
 bind_interrupts!(struct Irua {
@@ -25,6 +26,55 @@ bind_interrupts!(struct Irua {
 });
 
 const GPS_BUF_SIZE: usize = 256; //starting with this till issues arise
+
+fn parse_gpgga(gpgga: [BufTxt; 15]) -> Option<Gps> {
+    let utc_time = gpgga[1];
+    let lat_raw = core::str::from_utf8(&gpgga[2].characters)
+        .unwrap()
+        .parse()
+        .ok()?;
+    let long_raw = core::str::from_utf8(&gpgga[4].characters)
+        .unwrap()
+        .parse()
+        .ok()?;
+    let altitude = gpgga[9];
+    let altitude_units = gpgga[10];
+
+    //Convert Latitude to decimal degrees
+    let lat_degrees = ((lat_raw / 100f64) as u64) as f64;
+    let lat_minutes = lat_raw % 100f64;
+    let mut latitude = lat_degrees + (lat_minutes / 60f64);
+    if gpgga[3] == BufTxt::from_str("S").unwrap() {
+        latitude *= -1;
+    }
+
+    //Convert Longitude to decimal degrees
+    let long_degrees = ((long_raw / 100f64) as u64) as f64;
+    let long_minutes = long_raw % 100f64;
+    let mut longitude = long_degrees + (long_minutes / 60f64);
+    if gpgga[3] == BufTxt::from_str("W").unwrap() {
+        longitude *= -1;
+    }
+    return Some(());
+
+    // latitude_hemisphere = fields[3]
+    // longitude_hemisphere = fields[5]
+
+    // # Convert latitude to decimal degrees
+    // lat_degrees = int(latitude_raw / 100)
+    // lat_minutes = latitude_raw % 100
+    // latitude = lat_degrees + (lat_minutes / 60)
+    // if latitude_hemisphere == 'S':
+    //     latitude *= -1
+
+    // # Convert longitude to decimal degrees
+    // lon_degrees = int(longitude_raw / 100)
+    // lon_minutes = longitude_raw % 100
+    // longitude = lon_degrees + (lon_minutes / 60)
+    // if longitude_hemisphere == 'W':
+    //     longitude *= -1
+    //
+}
 
 #[embassy_executor::task]
 pub async fn init_gps(
@@ -53,33 +103,6 @@ pub async fn init_gps(
                     == ['G' as u8, 'P' as u8, 'G' as u8, 'G' as u8, 'A' as u8]
                     && split_chunk[14] != BufTxt::default()
                 {
-                    //TODO
-                    // utc_time = fields[1]
-                    // latitude_raw = float(fields[2])
-                    // latitude_hemisphere = fields[3]
-                    // longitude_raw = float(fields[4])
-                    // longitude_hemisphere = fields[5]
-                    // gps_quality = int(fields[6])
-                    // num_satellites = int(fields[7])
-                    // hdop = float(fields[8])
-                    // altitude = float(fields[9])
-                    // altitude_units = fields[10]
-
-                    // # Convert latitude to decimal degrees
-                    // lat_degrees = int(latitude_raw / 100)
-                    // lat_minutes = latitude_raw % 100
-                    // latitude = lat_degrees + (lat_minutes / 60)
-                    // if latitude_hemisphere == 'S':
-                    //     latitude *= -1
-
-                    // # Convert longitude to decimal degrees
-                    // lon_degrees = int(longitude_raw / 100)
-                    // lon_minutes = longitude_raw % 100
-                    // longitude = lon_degrees + (lon_minutes / 60)
-                    // if longitude_hemisphere == 'W':
-                    //     longitude *= -1
-
-                    //
                     for i in 0..15 {
                         println!(
                             "[{}]: {}",
